b2ae2e0f4929cdb9efd0e4eb276d183cd5dcc1fd
johnny <johnny@wantgoo.com>
2018-10-06 16:39:27 +0800

    first commit

---
+++ Chef.Data/Chef.Data/Extension.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace Chef.Data
+{
+    public static class Extension
+    {
+        public static string GenerateUpdateCommand(this object me, out Dictionary<string, object> parameters)
+        {
+            parameters = new Dictionary<string, object>();
+
+            var table = (string)me.GetType()
+                .CustomAttributes.Single(x => x.AttributeType == typeof(TableAttribute))
+                .ConstructorArguments[0]
+                .Value;
+
+            var conditions = new List<string>();
+            var setters = new List<string>();
+
+            foreach (var property in me.GetType().GetProperties())
+            {
+                var customColumn =
+                    property.CustomAttributes.SingleOrDefault(x => x.AttributeType == typeof(ColumnAttribute));
+
+                var columnName = customColumn != null
+                                     ? (string)customColumn.ConstructorArguments[0].Value
+                                     : property.Name;
+
+                var field = property.GetValue(me);
+
+                if (field is Field)
+                {
+                    setters.Add($"[{columnName}] = @{property.Name}");
+                    parameters.Add(property.Name, ((Field)field).GetValue());
+                }
+                else
+                {
+                    conditions.Add($"[{columnName}] = @{property.Name}");
+                    parameters.Add(property.Name, field);
+                }
+            }
+
+            return @"
+UPDATE [" + table + @"]
+SET " + string.Join(", ", setters) + @"
+WHERE " + string.Join(" AND ", conditions);
+        }
+    }
+}
---
+++ Chef.Data/Chef.Data/Field.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Chef.Data
+{
+    public abstract class Field
+    {
+        public abstract Type GetValueType();
+
+        public abstract object GetValue();
+    }
+
+    public class Field<T> : Field
+    {
+        public Field(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; }
+
+        public static implicit operator Field<T>(T value)
+        {
+            return new Field<T>(value);
+        }
+
+        public override Type GetValueType()
+        {
+            return typeof(T);
+        }
+
+        public override object GetValue()
+        {
+            return this.Value;
+        }
+    }
+}
