2769b53b65cefdd2050de4f3fe9cf23cef5c3d68
johnny <johnny@wantgoo.com>
2019-03-11 16:52:02 +0800

    update Chef.Data to 2.0.0

--- Chef.Data/Chef.Data/Extension.cs
+++ Chef.Data/Chef.Data/Extension.cs
@@ -9,10 +9,31 @@ namespace Chef.Data
     public static class Extension
     {
         public static string GenerateUpdateCommand(
-            this object me,
+            this IFieldSet me,
             out IEnumerable<KeyValuePair<string, object>> parameters)
         {
             return GenerateUpdateCommand(me, string.Empty, out parameters);
+        }
+
+        public static string GenerateUpdateCommand(
+            this IEnumerable<IFieldSet> me,
+            out IEnumerable<KeyValuePair<string, object>> parameters)
+        {
+            var dict = new Dictionary<string, object>();
+            var output = new StringBuilder();
+
+            var index = 0;
+
+            foreach (var item in me)
+            {
+                output.AppendLine(GenerateUpdateCommand(item, $"_{index++}", out var tmpParameters));
+
+                if (tmpParameters != null) dict.AddRange(tmpParameters);
+            }
+
+            parameters = dict.Count > 0 ? dict : null;
+
+            return output.ToString();
         }
 
         private static string GenerateUpdateCommand(
@@ -22,62 +43,48 @@ namespace Chef.Data
         {
             var dict = new Dictionary<string, object>();
             var output = new StringBuilder();
-            
-            if (me is IEnumerable enumerable)
+
+            var tableName = (string)me.GetType()
+                .CustomAttributes.Single(x => x.AttributeType == typeof(TableAttribute))
+                .ConstructorArguments[0]
+                .Value;
+
+            var conditions = new List<string>();
+            var setters = new List<string>();
+
+            foreach (var property in me.GetType().GetProperties())
             {
-                var index = 0;
+                if (property.CustomAttributes.Any(x => x.AttributeType == typeof(NotMappedAttribute))) continue;
 
-                foreach (var item in enumerable)
+                var customColumn =
+                    property.CustomAttributes.SingleOrDefault(x => x.AttributeType == typeof(ColumnAttribute));
+
+                var columnName = customColumn != null
+                                     ? (string)customColumn.ConstructorArguments[0].Value
+                                     : property.Name;
+
+                var parameterName = string.Concat(property.Name, suffix);
+                var parameterValue = property.GetValue(me);
+
+                switch (parameterValue)
                 {
-                    output.AppendLine(GenerateUpdateCommand(item, $"_{index++}", out var tmpParameters));
+                    case null: continue;
 
-                    if (tmpParameters != null) dict.AddRange(tmpParameters);
+                    case Field field:
+                        setters.Add($"[{columnName}] = @{parameterName}");
+                        dict.Add(parameterName, field.GetValue());
+                        break;
+
+                    default:
+                        conditions.Add($"[{columnName}] = @{parameterName}");
+                        dict.Add(parameterName, parameterValue);
+                        break;
                 }
             }
-            else
-            {
-                var tableName = (string)me.GetType()
-                    .CustomAttributes.Single(x => x.AttributeType == typeof(TableAttribute))
-                    .ConstructorArguments[0]
-                    .Value;
 
-                var conditions = new List<string>();
-                var setters = new List<string>();
-
-                foreach (var property in me.GetType().GetProperties())
-                {
-                    if (property.CustomAttributes.Any(x => x.AttributeType == typeof(NotMappedAttribute))) continue;
-
-                    var customColumn =
-                        property.CustomAttributes.SingleOrDefault(x => x.AttributeType == typeof(ColumnAttribute));
-
-                    var columnName = customColumn != null
-                                         ? (string)customColumn.ConstructorArguments[0].Value
-                                         : property.Name;
-
-                    var parameterName = string.Concat(property.Name, suffix);
-                    var parameterValue = property.GetValue(me);
-
-                    switch (parameterValue)
-                    {
-                        case null: continue;
-
-                        case Field field:
-                            setters.Add($"[{columnName}] = @{parameterName}");
-                            dict.Add(parameterName, field.GetValue());
-                            break;
-
-                        default:
-                            conditions.Add($"[{columnName}] = @{parameterName}");
-                            dict.Add(parameterName, parameterValue);
-                            break;
-                    }
-                }
-
-                output.AppendLine($"UPDATE [{tableName}]");
-                output.AppendLine($"SET {string.Join(", ", setters)}");
-                output.AppendLine($"WHERE {string.Join(" AND ", conditions)};");
-            }
+            output.AppendLine($"UPDATE [{tableName}]");
+            output.AppendLine($"SET {string.Join(", ", setters)}");
+            output.AppendLine($"WHERE {string.Join(" AND ", conditions)};");
 
             parameters = dict.Count > 0 ? dict : null;
 
